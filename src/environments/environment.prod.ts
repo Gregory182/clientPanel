e39934a24472625d1f3b7d3ae3e608a632d9714d
Gregory182 <rozpedzik@gmail.com>
2018-08-23 15:34:27 +0200

    init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiPpzPMFrPt--wrIVcujqDaecxLkj1yxo",
+    authDomain: "clientpanelprod-10944.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-10944.firebaseio.com",
+    projectId: "clientpanelprod-10944",
+    storageBucket: "clientpanelprod-10944.appspot.com",
+    messagingSenderId: "237859711653"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDiPpzPMFrPt--wrIVcujqDaecxLkj1yxo",
+    authDomain: "clientpanelprod-10944.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-10944.firebaseio.com",
+    projectId: "clientpanelprod-10944",
+    storageBucket: "clientpanelprod-10944.appspot.com",
+    messagingSenderId: "237859711653"
+  }
 };
 
 /*
